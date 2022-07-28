c5c3229ff81df2016ccd4e615a1efed7398390cb
Ugi <ugurcank@gmail.com>
2022-07-28 15:50:08 -0700

    Initial Commit

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  },
+};
+
+export default config;
