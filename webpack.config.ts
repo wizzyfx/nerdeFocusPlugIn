0a410e2640772b1767736b6987c86d7c7f3095d4
Ugi <ugurcank@gmail.com>
2022-08-11 15:49:05 -0700

    Initial Commit

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,32 @@
+import CopyWebpackPlugin from "copy-webpack-plugin";
 import path from "path";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import type { Configuration } from "webpack";
+import { version } from "./package.json";
 
 const devServer: DevServerConfiguration = {
   static: path.join(__dirname, "build"),
   compress: true,
   port: 4000,
 };
+
+const createManifest = new CopyWebpackPlugin({
+  patterns: [
+    {
+      from: path.join(__dirname, "src", "manifest", "manifest.json"),
+      to: path.join(__dirname, "build", "manifest.json"),
+      force: true,
+      transform: function (content, path) {
+        return Buffer.from(
+          JSON.stringify({
+            ...JSON.parse(content.toString()),
+            version: version,
+          })
+        );
+      },
+    },
+  ],
+});
 
 const config: Configuration = {
   entry: { inject: "./src/content/index.ts" },
@@ -36,6 +56,7 @@ const config: Configuration = {
     path: path.resolve(__dirname, "build"),
     filename: "[name].js",
   },
+  plugins: [createManifest],
   devServer,
 };
 
