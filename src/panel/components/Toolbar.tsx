b40f6714e739319b311866363d51c4f35c2dc824
Ugi <ugurcank@gmail.com>
2022-08-19 15:58:13 -0700

    0.5.1

--- src/panel/components/Toolbar.tsx
+++ src/panel/components/Toolbar.tsx
@@ -1,52 +1,14 @@
 import React from "react";
 import { useFocus, FocusScope, useFocusManager } from "react-aria";
 import "../index.less";
-
-function ToolbarButton(props: any) {
-  const [focusable, setFocusable] = React.useState(
-    props.tabIndex === 0 ? true : false
-  );
-  const focusManager = useFocusManager();
-
-  const { focusProps } = useFocus({
-    onFocus: (e) => setFocusable(true),
-    onBlur: (e) => setFocusable(false),
-  });
-
-  const onKeyDown = (e: { key: any }) => {
-    switch (e.key) {
-      case "ArrowRight":
-        focusManager.focusNext({ wrap: true, tabbable: false }).tabIndex = 0;
-        break;
-      case "ArrowLeft":
-        focusManager.focusPrevious({ wrap: true, tabbable: false });
-        break;
-      case "Home":
-        focusManager.focusFirst({ wrap: true, tabbable: false });
-        break;
-      case "End":
-        focusManager.focusLast({ wrap: true, tabbable: false });
-        break;
-    }
-  };
-
-  return (
-    <button
-      tabIndex={focusable ? 0 : -1}
-      onKeyDown={onKeyDown}
-      {...props}
-      {...focusProps}
-    >
-      1
-    </button>
-  );
-}
+import "./Toolbar.less";
+import ToolbarButton from "./ToolbarButton";
 
 const Toolbar: React.FC = () => {
   return (
     <div className={`toolbar`} role="toolbar" aria-label="NerdeFocus Controls">
       <FocusScope>
-        <ToolbarButton tabIndex={0} />
+        <ToolbarButton />
         <ToolbarButton />
         <ToolbarButton />
       </FocusScope>
---
+++ src/panel/components/ToolbarButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../index.less";
+
+function ToolbarButton(props: any) {
+  return <button>2</button>;
+}
+
+export default ToolbarButton;
